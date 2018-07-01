48bf7019b15eeddccba98aece09c72e902e22a7b
bdeore4 <bdeore4@gmail.com>
2018-07-01 19:01:36 +0530

    Remove leakCanary

--- presentation/src/main/java/com/bhushandeore/starwars/sample/presentation/AndroidApplication.java
+++ presentation/src/main/java/com/bhushandeore/starwars/sample/presentation/AndroidApplication.java
@@ -1,38 +1,32 @@
 package com.bhushandeore.starwars.sample.presentation;
 
 import android.app.Application;
+
 import com.bhushandeore.starwars.sample.presentation.di.components.ApplicationComponent;
 import com.bhushandeore.starwars.sample.presentation.di.components.DaggerApplicationComponent;
 import com.bhushandeore.starwars.sample.presentation.di.modules.ApplicationModule;
-import com.bhushandeore.starwars.sample.presentation.BuildConfig;
-import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Android Main Application
  */
 public class AndroidApplication extends Application {
 
-  private ApplicationComponent applicationComponent;
+    private ApplicationComponent applicationComponent;
 
-  @Override public void onCreate() {
-    super.onCreate();
-    this.initializeInjector();
-    this.initializeLeakDetection();
-  }
-
-  private void initializeInjector() {
-    this.applicationComponent = DaggerApplicationComponent.builder()
-        .applicationModule(new ApplicationModule(this))
-        .build();
-  }
-
-  public ApplicationComponent getApplicationComponent() {
-    return this.applicationComponent;
-  }
-
-  private void initializeLeakDetection() {
-    if (BuildConfig.DEBUG) {
-      LeakCanary.install(this);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.initializeInjector();
     }
-  }
+
+    private void initializeInjector() {
+        this.applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return this.applicationComponent;
+    }
+
 }
